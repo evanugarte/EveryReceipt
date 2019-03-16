be7df6cf67d61ff454df88cd7c5b6b353a92e064
Ricky Singh <singh.sangh@gmail.com>
2019-03-15 19:47:50 -0700

    Creates Add button

--- src/components/HomeScreen/HomeScreen.js
+++ src/components/HomeScreen/HomeScreen.js
@@ -1,28 +1,29 @@
+
 import React from "react";
-import { Text, Button } from "react-native";
+import { Text, Button, StyleSheet } from "react-native";
+import  AddButton  from './AddButton.js';
+
 
 export default class Title extends React.Component {
 
 
-  async onLogout() {
-    this.props.navigation.navigate("LoggedOut");
-  }
+    async onLogout() {
+        this.props.navigation.navigate("LoggedOut");
+    }
 
-  async goToProfile() {
-    this.props.navigation.navigate("Profile");
-  }
+    async goToProfile() {
+        this.props.navigation.navigate("Profile");
+    }
 
-  render() {
-    return(
-      <React.Fragment>
-        <Button
-          title="Logout"
-          onPress={this.onLogout.bind(this)} />
-        <Button
-          title="Profile"
-          onPress={this.goToProfile.bind(this)} />
-        <Text>Welcome to EveryReceipt</Text>
-      </React.Fragment>
-    );
-  }
+    render() {
+        return(
+            <React.Fragment>
+                <AddButton/>
+                <Button
+                    title="Logout"
+                    onPress={this.onLogout.bind(this)} />
+            </React.Fragment>
+        );
+    }
 }
+
