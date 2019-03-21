ddff2a9019e97bf168f93915721f15ff0feb1bce
Ricky Singh <singh.sangh@gmail.com>
2019-03-20 19:48:06 -0700

    Fixes lint errors

--- src/components/HomeScreen/HomeScreen.js
+++ src/components/HomeScreen/HomeScreen.js
@@ -1,29 +1,29 @@
 
 import React from "react";
 import { Text, Button, StyleSheet } from "react-native";
-import  AddButton  from './AddButton.js';
+import  AddButton  from "./AddButton.js";
 
 
 export default class Title extends React.Component {
 
 
-    async onLogout() {
-        this.props.navigation.navigate("LoggedOut");
-    }
+  async onLogout() {
+    this.props.navigation.navigate("LoggedOut");
+  }
 
-    async goToProfile() {
-        this.props.navigation.navigate("Profile");
-    }
+  async goToProfile() {
+    this.props.navigation.navigate("Profile");
+  }
 
-    render() {
-        return(
-            <React.Fragment>
-                <AddButton/>
-                <Button
-                    title="Logout"
-                    onPress={this.onLogout.bind(this)} />
-            </React.Fragment>
-        );
-    }
+  render() {
+    return(
+      <React.Fragment>
+        <AddButton/>
+        <Button
+          title="Logout"
+          onPress={this.onLogout.bind(this)} />
+      </React.Fragment>
+    );
+  }
 }
 
