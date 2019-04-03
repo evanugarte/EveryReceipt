803f14561fcc855b63c896f37d5637f64a31e550
Evan Ugarte <evanuxd@gmail.com>
2019-04-03 12:07:55 -0700

    Added Ignore Warning for Timer
    The warning arises from using firebase services. A yellow box appears at
    the bottom of the screen, now it's ignored.

--- App.js
+++ App.js
@@ -2,6 +2,10 @@ import React from "react";
 import AppNavigator from "./src/navigator/AppNavigator";
 import { Provider } from "react-redux";
 import store from "./src/store";
+import { YellowBox } from "react-native";
+import _ from "lodash";
+
+YellowBox.ignoreWarnings(["Setting a timer"]);
 
 export default class App extends React.Component {
 
