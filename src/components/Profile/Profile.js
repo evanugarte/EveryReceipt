e7f77a43fe32bf7985ade55c929a38e427af9e68
Evan Ugarte <evanuxd@gmail.com>
2019-04-05 14:44:12 -0700

    Added User Profile Page
    This page simply shows the user's name. This will be made to look
    prettier in the future.

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,16 +1,13 @@
 import React from "react";
-import { View, StyleSheet, Button, Text } from "react-native";
+import { View, Button, Text } from "react-native";
 import { connect } from "react-redux";
 import UserInfo from "./UserInfo";
+import { styles } from "./styles";
 
 class Profile extends React.Component {
   constructor(props) {
     super(props);
     this.onLogout = this.onLogout.bind(this);
-  }
-  
-  async onLogout() {
-    this.props.navigation.navigate("LoggedOut");
   }
   
   async goToHome() {
@@ -22,11 +19,7 @@ class Profile extends React.Component {
     return (
       <View style={styles.base}>
         <UserInfo profile={profile} />
-        <Button
-          title="Logout"
-          onPress={this.onLogout.bind(this)} />
         <Button title="Home" onPress={this.goToHome.bind(this)} />
-        <Text>We out here</Text>
       </View>
     );
   }
