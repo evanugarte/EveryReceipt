1049d3c0ee1b337fd37ed11447619960af3ef947
Evan Ugarte <evanuxd@gmail.com>
2019-04-05 10:00:36 -0700

    Fixed Bug to Store User Info
    This happened because I misspelled uid. I put "id" instead. I also added
    the capability for profile info to be shown such as name and initial.
    Weee

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,23 +1,27 @@
 import React from "react";
 import { View, StyleSheet, Button, Text } from "react-native";
+import { connect } from "react-redux";
+import UserInfo from "./UserInfo";
 
-export default class Profile extends React.Component {
+class Profile extends React.Component {
   constructor(props) {
     super(props);
     this.onLogout = this.onLogout.bind(this);
   }
-
+  
   async onLogout() {
     this.props.navigation.navigate("LoggedOut");
   }
-
+  
   async goToHome() {
     this.props.navigation.navigate("HomeScreen");
   }
-
+  
   render() {
+    const { profile } = this.props;
     return (
       <View style={styles.base}>
+        <UserInfo profile={profile} />
         <Button
           title="Logout"
           onPress={this.onLogout.bind(this)} />
@@ -28,10 +32,11 @@ export default class Profile extends React.Component {
   }
 }
 
-const styles = StyleSheet.create({
-  base: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center"
-  }
-});
+const mapStateToProps = (state) => {
+  return {
+    profile: state.firebase.profile
+  };
+};
+
+export default connect(mapStateToProps)(Profile);
+
