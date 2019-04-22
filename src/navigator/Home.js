f52222212e072dca5f5322e8a597e1cd54707cc6
Ricky Singh <singh.sangh@gmail.com>
2019-04-22 01:03:20 -0700

    Homescreen fix

--- src/navigator/Home.js
+++ src/navigator/Home.js
@@ -1,32 +1,9 @@
 import { createSwitchNavigator } from "react-navigation";
-
 import SearchScreen from "../components/SearchScreen/SearchScreen";
 import ItemEdit from "../components/ItemEdit/ItemEdit";
 import HomeScreen from "../components/HomeScreen/HomeScreen";
 import ManualAddScreen from "../components/ItemEntry/ManualAddScreen";
-<<<<<<< 571f0a8faa685355a2c48ecd5261ec95031b6355
-<<<<<<< 6705db628c12c968bd700095d0acbb17f04fad1a
-<<<<<<< 6bbbb384b69ebf0c158d687f638788946371e177
-=======
-<<<<<<< 39ebadd2fd5c803702fcdedfc3a3416b8ad61bee
->>>>>>> Moves camera code into HomeScreen
-import Profile from "../components/Profile/Profile";
-<<<<<<< bcc80225dad030c43352df0ca70aa997a2fc3a91
-=======
-import OpenCameraRoll from "../components/ItemEntry/OpenCameraRoll";
-import OpenCamera from "../components/ItemEntry/OpenCamera";
-<<<<<<< 571f0a8faa685355a2c48ecd5261ec95031b6355
->>>>>>> Adds OpenCamera and OpenCameraRoll
-=======
->>>>>>> Moves camera code into HomeScreen
-=======
-import OpenCameraRoll from "../components/ItemEntry/OpenCameraRoll";
-import OpenCamera from "../components/ItemEntry/OpenCamera";
->>>>>>> Adds OpenCamera and OpenCameraRoll
-=======
-=======
->>>>>>> Moves camera code into HomeScreen
->>>>>>> Moves camera code into HomeScreen
+
 
 /**
  * Create navigator for the home screen, which contains navigations to
@@ -53,18 +30,6 @@ const HomeNavigator = createSwitchNavigator({
   ManualAddScreen: {
     screen: ManualAddScreen
   },
-<<<<<<< 571f0a8faa685355a2c48ecd5261ec95031b6355
-<<<<<<< bcc80225dad030c43352df0ca70aa997a2fc3a91
-=======
-  OpenCameraRollPage: {
-    screen: OpenCameraRoll
-  },
-  OpenCameraPage: {
-    screen: OpenCamera
-  }
->>>>>>> Adds OpenCamera and OpenCameraRoll
-=======
->>>>>>> Moves camera code into HomeScreen
 });
 
 export default HomeNavigator;
