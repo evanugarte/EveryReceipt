0531512545f8178dd267de6a25758395a1844514
Ricky Singh <singh.sangh@gmail.com>
2019-05-08 16:15:33 -0700

    Removes unnecessary imports

--- src/components/SearchScreen/SearchBar.js
+++ src/components/SearchScreen/SearchBar.js
@@ -6,7 +6,6 @@ import {
   View, TouchableOpacity
 } from "react-native";
 import { styles } from "../Common/styles";
-import CommonButton from "../Common/CommonButton";
 
 export default class SearchBar extends React.Component {
   constructor(props) {
